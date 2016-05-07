f35b7234d25a9bb7983f6747d8cc6dac0e5a14e9
Harshith Keni <harshith.keni@sdstate.edu>
2016-05-07 16:23:06 -0500

    the list model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :lists
+  resources :lists
+  resources :lists
   devise_for :users
   root to: 'devise/sessions#new'
   # The priority is based upon order of creation: first created -> highest priority.
