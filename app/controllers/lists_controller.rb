0770bf6c5ffe60812f5acddc5090cd054132c402
Harshith Keni <harshith.keni@sdstate.edu>
2016-05-08 18:16:06 -0500

    create a new list

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -4,4 +4,23 @@ class ListsController < ApplicationController
   def index
     @lists = current_user.lists
   end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to '/', flash: {success: 'List created successfully'}
+    else
+      redirect_to '/', flash: {error: @list.errors}
+    end
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:title, :description, :list_items)
+  end
 end
